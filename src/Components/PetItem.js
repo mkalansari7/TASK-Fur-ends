c85911ec0093242f5268af8a81b841971cd8fcd4
mkalansari7 <mk.alansari7@gmail.com>
2022-01-17 20:20:28 +0300

    done without the challenge

--- src/Components/PetItem.js
+++ src/Components/PetItem.js
@@ -1,12 +1,18 @@
+import React, { useState } from "react";
 export default function PetItem(props) {
   const pet = props.pet;
+  const [image, setImage] = useState(pet.image);
+
+  const onChangeImage = () => {
+    setImage(pet.image2);
+  };
   return (
     <div class="col-lg-4 col-md-8 col-sm-10">
       <div class="single-doctor">
-        <img className="image" alt={pet.name} src={pet.image} />
+        <img className="image" alt={pet.name} src={image} />
         <div class="content">
           <h3>{pet.name}</h3>
-          <button type="button" class="btn btn-info">
+          <button onClick={onChangeImage} type="button" class="btn btn-info">
             Pet
           </button>
         </div>
--- src/Components/PetsList.js
+++ src/Components/PetsList.js
@@ -1,7 +1,24 @@
-import PetItem from './PetItem';
+import React, { useState } from "react";
+import PetItem from "./PetItem";
 
 export default function PetsList(props) {
-  const pets = props.pets.map((pet) => <PetItem key={pet.id} pet={pet} />);
+  const [query, setQuery] = useState("");
+  const [type, setType] = useState("");
+  const pets = props.pets
+    .filter(
+      (pet) =>
+        pet.name.toLowerCase().includes(query.toLowerCase()) &&
+        pet.type.includes(type)
+    )
+    .map((pet) => <PetItem key={pet.id} pet={pet} />);
+
+  const onChangeSearch = (e) => {
+    setQuery(e.target.value);
+  };
+  const changePetSelctor = (event) => {
+    setType(event.target.value);
+  };
+
   return (
     <section id="doctors" class="doctor-section pt-140">
       <div class="container">
@@ -18,11 +35,12 @@ export default function PetsList(props) {
                   placeholder="Search"
                   aria-label="Search"
                   aria-describedby="search-addon"
+                  onChange={onChangeSearch}
                 />
               </div>
               <br />
               Type:
-              <select class="form-select">
+              <select onChange={changePetSelctor} class="form-select">
                 <option value="" selected>
                   All
                 </option>
